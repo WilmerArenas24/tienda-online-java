e378a39a63b6f5e429fc917197d08d81832b8bd2
Wilmer Arenas <wilmerarenas24@gmail.com>
2025-01-27 19:40:38 -0500

    Proyecto inicial en spring

---
+++ tienda_online_pc/src/main/java/tiendaOnlinePC/tienda_online_pc/TiendaOnlinePcApplication.java
@@ -0,0 +1,13 @@
+package tiendaOnlinePC.tienda_online_pc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaOnlinePcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaOnlinePcApplication.class, args);
+	}
+
+}
---
+++ tienda_online_pc/src/test/java/tiendaOnlinePC/tienda_online_pc/TiendaOnlinePcApplicationTests.java
@@ -0,0 +1,13 @@
+package tiendaOnlinePC.tienda_online_pc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaOnlinePcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
